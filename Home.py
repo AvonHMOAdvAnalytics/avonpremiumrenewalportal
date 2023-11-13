a3b23edb6de1ba8c2b31f18f49a7008d05f246ea
longman86 <ademola.atolagbe@avonhealthcare.com>
2023-11-13 14:16:26 +0100

    updated the DB cred

--- Home.py
+++ Home.py
@@ -5,6 +5,7 @@ import numpy as np
 import datetime as dt
 from PIL import Image
 import textwrap
+import os
 
 
 st.set_page_config(page_title= 'Premium Calculator',layout='wide', initial_sidebar_state='expanded')
@@ -15,16 +16,32 @@ st.image(image, use_column_width=False)
 query = 'select * from tblEnrolleePremium'
 query1 = 'select * from vw_tbl_client_mlr'
 
+server = os.environ.get('server_name')
+database = os.environ.get('db_name')
+username = os.environ.get('db_username')
+password = os.environ.get('db_password')
+
 conn = pyodbc.connect(
         'DRIVER={ODBC Driver 17 for SQL Server};SERVER='
-        +st.secrets['server']
+        + server
         +';DATABASE='
-        +st.secrets['database']
+        + database
         +';UID='
-        +st.secrets['username']
+        + username
         +';PWD='
-        +st.secrets['password']
-        ) 
+        + password
+        )
+
+# conn = pyodbc.connect(
+#         'DRIVER={ODBC Driver 17 for SQL Server};SERVER='
+#         +st.secrets['server']
+#         +';DATABASE='
+#         +st.secrets['database']
+#         +';UID='
+#         +st.secrets['username']
+#         +';PWD='
+#         +st.secrets['password']
+#         ) 
 
 @st.cache_data(ttl = dt.timedelta(hours=24))
 def get_data_from_sql():  
