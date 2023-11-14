da72ace25de4e32f6fc5864898f6b2e7d4d13a20
longman86 <ademola.atolagbe@avonhealthcare.com>
2023-11-14 10:57:42 +0100

    modified the plan and client information logic

--- Home.py
+++ Home.py
@@ -16,32 +16,32 @@ st.image(image, use_column_width=False)
 query = 'select * from tblEnrolleePremium'
 query1 = 'select * from vw_tbl_client_mlr'
 
-server = os.environ.get('server_name')
-database = os.environ.get('db_name')
-username = os.environ.get('db_username')
-password = os.environ.get('db_password')
-
-conn = pyodbc.connect(
-        'DRIVER={ODBC Driver 17 for SQL Server};SERVER='
-        + server
-        +';DATABASE='
-        + database
-        +';UID='
-        + username
-        +';PWD='
-        + password
-        )
+# server = os.environ.get('server_name')
+# database = os.environ.get('db_name')
+# username = os.environ.get('db_username')
+# password = os.environ.get('db_password')
 
 # conn = pyodbc.connect(
 #         'DRIVER={ODBC Driver 17 for SQL Server};SERVER='
-#         +st.secrets['server']
+#         + server
 #         +';DATABASE='
-#         +st.secrets['database']
+#         + database
 #         +';UID='
-#         +st.secrets['username']
+#         + username
 #         +';PWD='
-#         +st.secrets['password']
-#         ) 
+#         + password
+#         )
+
+conn = pyodbc.connect(
+        'DRIVER={ODBC Driver 17 for SQL Server};SERVER='
+        +st.secrets['server']
+        +';DATABASE='
+        +st.secrets['database']
+        +';UID='
+        +st.secrets['username']
+        +';PWD='
+        +st.secrets['password']
+        ) 
 
 @st.cache_data(ttl = dt.timedelta(hours=24))
 def get_data_from_sql():  
@@ -61,8 +61,18 @@ def generate_input_fields(client, plan_names):
         f_num_lives = st.number_input(f'Number of Family Lives on {plan_name}', min_value=0)
         f_premium_paid = st.number_input(f'Premium for Family Lives on {plan_name}', min_value=20000)
         total_lives = st.number_input(f'Input the total number of Lives on {plan_name}', min_value=1)
+        actual_premium = st.number_input(f'Input the actual total premium paid on {plan_name}', min_value=20000)
+        repriced_key = f'{client}_{plan_name}_repriced'
+        repriced = st.radio(label='Has this plan been repriced in the Last 3 Years?', key=repriced_key, options=['No', 'Yes'])
+        if repriced == 'Yes':
+            repriced_yr = st.number_input(label=f'What year was {plan_name} repriced?',min_value=2019, max_value=dt.date.today().year)
+            repriced_percent = st.number_input(label=f'By what % was {plan_name} repriced?')
+        else:
+            repriced_yr = 0
+            repriced_percent = 0
         plan_data.append({'client': client, 'plan_name': plan_name, 'i_num_lives': i_num_lives, 'f_num_lives': f_num_lives, 
-                           'i_premium_paid': i_premium_paid, 'f_premium_paid': f_premium_paid,'total_lives': total_lives})
+                           'i_premium_paid': i_premium_paid, 'f_premium_paid': f_premium_paid,'total_lives': total_lives,
+                           'actual_premium':actual_premium, 'repriced':repriced, 'repriced_yr':repriced_yr, 'repriced_percent':repriced_percent})
     return plan_data
 
 def reset_data():
@@ -104,19 +114,19 @@ plan_df = pd.DataFrame(plan_data)
 
 
 total_premium = (plan_df['i_num_lives'] * plan_df['i_premium_paid']) + (plan_df['f_num_lives'] * plan_df['f_premium_paid'])
-total_premium = total_premium.sum()
+total_calc_premium = total_premium.sum()
 total_lives = plan_df['total_lives'].sum()
+total_actual_premium = plan_df['actual_premium'].sum()
 
-st.subheader('Provide More Information Below About the Client')
+st.subheader('Provide Additional Information Below About the Client')
 
 year_joined = st.number_input(label='Client Onboarding Year',min_value=2013, max_value=dt.date.today().year)
 shared_portfolio = st.radio(label='Is this a shared portfolio?', options=['No', 'Yes'])
 if shared_portfolio == 'Yes':
     competitor = st.text_input(label='List the HMOs we are sharing the portfolio with', help='If more than one, seperate the names with comma')
-repriced = st.radio(label='Was the client repriced in the Last 3 Years?', options=['No', 'Yes'])
-if repriced == 'Yes':
-    repriced_yr = st.number_input(label='What year was the client repriced?',min_value=2018, max_value=dt.date.today().year)
-    repriced_percent = st.number_input(label='By what % was the client repriced?')
+else:
+    competitor = None
+
 notes = st.text_area(label='Additional Notes/Remarks')
 
 
@@ -157,16 +167,23 @@ client_mlr = round((client_medicalcost/client_revenue)*100, 2)
 if st.button('Submit'):
     cursor = conn.cursor()
     for plan in plan_data:
-        cursor.execute('insert into [dbo].[plan_renewal_template_data] (PolicyNo, client, plan_name, individual_lives, individual_premium, family_lives, family_premium, total_lives, date_submitted)\
-                       VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)',
-                       (policyno, client, plan['plan_name'], plan['i_num_lives'], plan['i_premium_paid'], plan['f_num_lives'], plan['f_premium_paid'], plan['total_lives'], dt.datetime.now())
+        cursor.execute('insert into [dbo].[plan_renewal_template_data]\
+                       (PolicyNo, client, plan_name, individual_lives, individual_premium, family_lives,\
+                        family_premium, total_lives, actual_premium, repriced_last_3yrs, year_repriced,\
+                        repriced_percent, date_submitted)\
+                       VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+                       (policyno, client, plan['plan_name'], plan['i_num_lives'], plan['i_premium_paid'],
+                        plan['f_num_lives'], plan['f_premium_paid'], plan['total_lives'], plan['actual_premium'],
+                        plan['repriced'], plan['repriced_yr'], plan['repriced_percent'], dt.datetime.now())
                        )
     
-    cursor.execute("insert into [dbo].[client_renewal_template_data] (PolicyNo, client, total_num_of_plans, total_lives, total_premium_paid, client_mlr,client_onboarding_yr,\
-                    shared_portfolio, competitor_HMO, repriced_last_3yrs, year_repriced, repriced_percent, policy_start_date, policy_end_date, AdditionalNotes,\
-                    date_submitted) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
-                   (policyno, client, int(num_plans), int(total_lives), int(total_premium), client_mlr, int(year_joined), shared_portfolio, competitor, repriced, int(repriced_yr),
-                     repriced_percent, start_date, end_date, notes, dt.datetime.now())
+    cursor.execute("insert into [dbo].[client_renewal_template_data]\
+                   (PolicyNo, client, total_num_of_plans, total_lives, total_calc_premium, \
+                   total_actual_premium, client_mlr, client_onboarding_yr, shared_portfolio, competitor_HMO,\
+                   policy_start_date, policy_end_date, AdditionalNotes, date_submitted)\
+                    VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
+                   (policyno, client, int(num_plans), int(total_lives), int(total_calc_premium), int(total_actual_premium),
+                    client_mlr, int(year_joined), shared_portfolio, competitor, start_date, end_date, notes, dt.datetime.now())
                    )
     conn.commit()
     st.success(f'All {client} Renewal Information Submitted Sucessfully')
